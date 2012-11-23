ee8aa3e82fa14652348c68a71f68c0bba40add21
Jordan Hewitt <jordannh@sent.com>
2012-11-22 18:35:02 -0800

    committing again.

--- port_scan.py
+++ port_scan.py
@@ -4,7 +4,15 @@ import pcapy
 import getopt, sys
 
 def generate_ip_list(pcap_src):
-  pcapfile = pcapy.open_offline(pcap_src)
+  reader = pcapy.open_offline(pcap_src)
+  print "Got a reader: ", reader
+  reader.setfilter("smtp.rsp || http.server || tcp.analysis.ack_rtt || dns.resp.addr || arp.opcode==reply")
+  while True:
+    (header, value) = reader.next()
+    if value == None:
+      pass
+    list_values.extend((head, value))
+  print "The list contains", len(list_values), "items"
 
 def throw_argv_error():
   print """
